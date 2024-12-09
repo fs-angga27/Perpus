388439ae9e0bf05c9f1806746a3509ed88fe193a
fxdagx27 <fldiangga27@gmail.com>
2024-12-09 11:46:54 +0700

    first commit

---
+++ lib/controllers/perpus_controller.dart
@@ -0,0 +1,22 @@
+// perpustakaan_controller.dart
+import 'package:perpus_/models/perpus.dart';
+
+class PerpustakaanController {
+  List<Perpus> bukuList = [];
+
+  void tambahBuku(Perpus buku) {
+    bukuList.add(buku);
+  }
+
+  void editBuku(int index, Perpus buku) {
+    bukuList[index] = buku;
+  }
+
+  void hapusBuku(int index) {
+    bukuList.removeAt(index);
+  }
+
+  List<Perpus> getBukuList() {
+    return bukuList;
+  }
+}
